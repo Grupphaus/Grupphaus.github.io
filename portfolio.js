a7bac6fb903e2e22c16f40953a5ad43392cb264b
Gruppenhaus <fellegrini@gmail.com>
2018-01-31 01:31:05 -0300

    first section styling. still needs some text. flex

--- portfolio.js
+++ portfolio.js
@@ -1,28 +1,30 @@
 
 window.onload = function(){
   var main = document.getElementById('container-main');
-  var scrollpos = main.scrollTop;
   var aboutHeader = document.getElementById("about");
   var proyectosHeader = document.getElementById("proyectos");
   var contactoHeader = document.getElementById("contacto");
-    main.addEventListener("scroll", function() {
-      if (scrollpos > 150) {
-        aboutHeader.className = "";
-      }
-  });
+  var about = document.getElementById("about-content");
+  var proyectos = document.getElementById("proyectos-content");
+  var contacto = document.getElementById("contacto-content");
+  var biosocial = document.querySelector(".bio-profiles");
+
   aboutHeader.addEventListener("click", function () {
     aboutHeader.className = "selected";
     proyectosHeader.className = "";
     contactoHeader.className = "";
+    biosocial.className = "bio-profiles";
   });
   proyectosHeader.addEventListener("click", function () {
-    proyectosHeader.className = "selected";
+    proyectosHeader.className = "selectedproy";
     aboutHeader.className = "";
     contactoHeader.className = "";
+    biosocial.className = "bio-profiles";
   });
   contactoHeader.addEventListener("click", function () {
     proyectosHeader.className = "";
-    contactoHeader.className = "selected";
+    biosocial.className = "bio-profiles biosocial";
+    contactoHeader.className = "selectedcont";
     aboutHeader.className = "";
   });
 };
