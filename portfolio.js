298955ec909c8306c4ec0ce7722eb64e2529b39f
Gruppenhaus <fellegrini@gmail.com>
2018-02-07 03:39:27 -0300

    some clean up

--- portfolio.js
+++ portfolio.js
@@ -9,24 +9,20 @@ window.onload = function(){
   var contacto = document.getElementById("contacto-content");
   var biosocial = document.querySelector(".bio-profiles");
   var portfolio = document.querySelector(".portfolio");
-  // var portfolioPage = document.querySelector(".portfolio-page");
 
   aboutHeader.addEventListener("click", function () {
     aboutHeader.className = "selected";
     proyectosHeader.className = "";
     contactoHeader.className = "";
-    biosocial.className = "bio-profiles";
   });
   proyectosHeader.addEventListener("click", function () {
     proyectosHeader.className = "selected";
     portfolio.className = "portfolio portfolio-animate";
     aboutHeader.className = "";
     contactoHeader.className = "";
-    biosocial.className = "bio-profiles";
   });
   contactoHeader.addEventListener("click", function () {
     proyectosHeader.className = "";
-    biosocial.className = "bio-profiles";
     contactoHeader.className = "selected";
     aboutHeader.className = "";
   });
