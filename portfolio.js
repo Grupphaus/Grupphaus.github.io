c511362c4e7c7e193f1e540670c292ca33ca4d2b
Gruppenhaus <fellegrini@gmail.com>
2018-03-10 04:53:43 -0300

    MAJOR functionality progress! jQuery to the rescue

--- portfolio.js
+++ portfolio.js
@@ -1,40 +1,42 @@
-window.onload = function(){
-  var main = document.getElementById('container-main');
-  var aboutHeader = document.getElementById("about");
-  var proyectosHeader = document.getElementById("proyectos");
-  var contactoHeader = document.getElementById("contacto");
-  var about = document.getElementById("about-content");
-  var proyectos = document.getElementById("proyectos-content");
-  var contacto = document.getElementById("contacto-content");
-  var portfolio = document.querySelector(".portfolio");
-  var portfolioPage = document.querySelectorAll("portfolio-page")
-  var hover = document.querySelectorAll('.hover');
-  var formContainer = document.querySelector(".form-container");
+$(document).ready(function(){
+  // Smooth scrolling
+  $("a").on("click", function(event) {
 
-  aboutHeader.addEventListener("click", function () {
-    aboutHeader.className = "selected";
-    proyectosHeader.className = "";
-    contactoHeader.className = "";
-  });
-  proyectosHeader.addEventListener("click", function () {
-    proyectosHeader.className = "selected";
-    portfolio.className = "portfolio portfolio-animate";
-    aboutHeader.className = "";
-    contactoHeader.className = "";
-  });
-  contactoHeader.addEventListener("click", function () {
-    contactoHeader.className = "selected";
-    formContainer.className = "form-container form-container-animate";
-    proyectosHeader.className = "";
-    aboutHeader.className = "";
+    if (this.hash !== "") {
+      // Prevent default
+      event.preventDefault();
+      // hash
+      var hash = this.hash;
+
+      $("html, body").animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        // default click behavior
+        window.location.hash = hash;
+      });
+    }
   });
 
+  // Nav element highlight toggle
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 600) {
+        $("#proyectos").addClass("selected");
+        $("#about").removeClass("selected");
+    } else {
+        $("#proyectos").removeClass("selected");
+        $("#about").addClass("selected");
+    }
+  });
+
+  var hover = $(".hover");
   for (var i = 0; i < hover.length; i++) {
-      hover[i].addEventListener('mouseover', function() {
-        this.style.opacity = "1";
-      });
-      hover[i].addEventListener('mouseleave', function() {
-        this.style.opacity = "0";
-      });
-  }
-};
+        hover[i].addEventListener('mouseover', function() {
+          this.style.opacity = "1";
+        });
+        hover[i].addEventListener('mouseleave', function() {
+          this.style.opacity = "0";
+        });
+    }
+});
