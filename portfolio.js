f6d5dafd56a108dd3534a279d71a9cfcacd1437d
Gruppenhaus <fellegrini@gmail.com>
2018-03-11 04:08:25 -0300

    added media queries. debugging browser resize glitches

--- portfolio.js
+++ portfolio.js
@@ -21,19 +21,61 @@ $(document).ready(function(){
   $(window).scroll(function() {
     var scroll = $(window).scrollTop();
 
-    if (scroll >= 600) {
+    if (scroll >= 500) {
         $("#proyectos").addClass("selected2");
         $("#about").removeClass("selected");
         $(".container-bio").css("background-color", "#017C69");
         $(".bio-profiles").css("background-color", "#003e34");
         $(".bio-name span").css("color", "#b2d7d2");
+
+        // if ($(window).width() <= 640) {
+        //   $("div.container-bio").toggleClass("hidden");
+        // }
+        //
+        // if ($(".bio-name").hasClass("hidden")) {
+        //   $(".bio-profiles").css("height", "100%");
+        //   $(".bio-name").display("none");
+        // } else if ($(".bio-name").hasClass("hidden") && $(window).height() > 360 && $(window).width() <= 640) {
+        //   $(".container-bio").css("height", "10%");
+        //   $(".bio-profiles").css("height", "100%");
+        // } else if ($(window).width() <= 640) {
+        //   if ($(window).height() <= 360) {
+        //     $(".container-bio").css("height", "40%");
+        //     $(".bio-profiles").css("height", "40%");
+        //   } else {
+        //     $(".container-bio").css("height", "25%");
+        //     $(".bio-profiles").css("height", "40%");
+        //   }
+        // }
+
     } else {
         $("#proyectos").removeClass("selected2");
         $("#about").addClass("selected");
         $(".container-bio").css("background-color", "#0288cf");
         $(".bio-profiles").css("background-color", "#01517c");
         $(".bio-name span").css("color", "#99cfeb");
+
+        // if ($(window).width() <= 640) {
+        //   $(".bio-name").removeClass("hidden");
+        // }
+        //
+        // if ($(".bio-name").hasClass("hidden") && $(window).height() <= 360) {
+        //   $(".container-bio").css("height", "15%");
+        //   $(".bio-profiles").css("height", "100%");
+        // } else if ($(".bio-name").hasClass("hidden") && $(window).height() > 360 && $(window).width() <= 640) {
+        //   $(".container-bio").css("height", "10%");
+        //   $(".bio-profiles").css("height", "100%");
+        // } else if ($(window).width() <= 640) {
+        //   if ($(window).height() <= 360) {
+        //     $(".container-bio").css("height", "40%");
+        //     $(".bio-profiles").css("height", "40%");
+        //   } else {
+        //     $(".container-bio").css("height", "25%");
+        //     $(".bio-profiles").css("height", "40%");
+        //   }
+        // }
     }
+
   });
 
   // Project detail containers (.hover) - toggling opacity on mouseover
